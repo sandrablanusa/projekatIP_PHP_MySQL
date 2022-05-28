dc8131d441df016998bed38c4e66b55f42848872
sandrablanusa <bozovicsandra3@gmail.com>
2022-05-28 21:03:42 +0200

    dodata lista aktivista kod admina

---
+++ administrator/listaAktivista.php
@@ -0,0 +1,17 @@
+<?php
+    $veza = new mysqli("localhost", "root", "");
+    $veza->set_charset("utf8");
+
+    $upit = "SELECT * FROM flajerisanje.aktivisti";
+
+    $rez = $veza->query($upit);
+    $brojRedova = mysqli_num_rows($rez);
+
+    for ($i=0; $i<$brojRedova; $i++){
+        $red = mysqli_fetch_array($rez);
+        echo "<li><div class=\"content\">".$red['ime']. " " .$red['prezime']."</div></li>";
+    }
+
+    $veza->close();
+
+ ?>
