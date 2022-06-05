dd6e610afd8058bafb4f16aac0d58af797c7b5e6
sandrablanusa <bozovicsandra3@gmail.com>
2022-06-06 01:34:10 +0200

    ispravljen bag

--- aktivista/listaFlajeraAktivista.php
+++ aktivista/listaFlajeraAktivista.php
@@ -8,34 +8,37 @@
     $rez = $veza->query($upit);
     $red = mysqli_fetch_array($rez);
     
-    $nizIdFlajera = array_map('intval', explode(',', $red['listaIDFlajera']));
-    $nizBrojeva = array_map('intval', explode(',', $red['listaBrojeva']));
+    if(!is_null($red['listaIDFlajera']) and !is_null($red['listaBrojeva'])){
 
-    $duzinaNiza = count($nizIdFlajera);
+        $nizIdFlajera = array_map('intval', explode(',', $red['listaIDFlajera']));
+        $nizBrojeva = array_map('intval', explode(',', $red['listaBrojeva']));
 
-    for ($i=0; $i<$duzinaNiza; $i++){
-        
-        $brFlajera =$nizBrojeva[$i];
-        $idFlajera =$nizIdFlajera[$i];
+        $duzinaNiza = count($nizIdFlajera);
 
-        $upit = "SELECT * FROM flajerisanje.flajeri WHERE idFlajera = '$idFlajera'";
-        $rez = $veza->query($upit);
-        $red = mysqli_fetch_array($rez);
+        for ($i=0; $i<$duzinaNiza; $i++){
+            
+            $brFlajera =$nizBrojeva[$i];
+            $idFlajera =$nizIdFlajera[$i];
 
-        echo "                <li>
-        <div class=\"flex-container\">
-            <div class=\"content\"><a href=\"../Flajerisanje/tabelaFlajeri/".$red['HTMLFajl']."\">".$red['naziv']."</a></div>
-            <div class=\"content\">Preostalo flajera: ".$brFlajera."</div>
-            <div class=\"dugmiciKontejner\">
-                <div class=\"delete\">
-                    <div class=\"button\">
-                        <button name=\"azuriraj\" type=\"submit\" value=\"".$idFlajera.",".$brFlajera."\">Unesi broj flajera</button>
+            $upit = "SELECT * FROM flajerisanje.flajeri WHERE idFlajera = '$idFlajera'";
+            $rez = $veza->query($upit);
+            $red = mysqli_fetch_array($rez);
+
+            echo "                <li>
+            <div class=\"flex-container\">
+                <div class=\"content\"><a href=\"../Flajerisanje/tabelaFlajeri/".$red['HTMLFajl']."\">".$red['naziv']."</a></div>
+                <div class=\"content\">Preostalo flajera: ".$brFlajera."</div>
+                <div class=\"dugmiciKontejner\">
+                    <div class=\"delete\">
+                        <div class=\"button\">
+                            <button name=\"azuriraj\" type=\"submit\" value=\"".$idFlajera.",".$brFlajera."\">Unesi broj flajera</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </li>";
-        
+        </li>";
+            
+        }
     }
 
     $veza->close();
